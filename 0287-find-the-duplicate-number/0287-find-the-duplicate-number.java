e9d202fedc202bc04c7ef932ea49efe01af95b6b
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-07-15 10:16:26 +0530

    Time: 5 ms (65.23%), Space: 57 MB (86.7%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        // hear & tortoise
         int slow = nums[0];
         int fast = nums[0];
 
@@ -12,12 +13,10 @@ class Solution {
         }
 
         slow = nums[0];
-
         while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
         }
-
         return fast;
     }
 }
