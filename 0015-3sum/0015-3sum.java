5a9875314275a732521456d0926c095f7a205140
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-07-20 10:36:49 +0530

    Time: 25 ms (96.03%), Space: 51.7 MB (48.47%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // int i = 0;
         if(nums.length < 3){
             return new ArrayList<>();
         }
-       List<List<Integer>> result = new ArrayList<>();
-       Arrays.sort(nums);
+        
+        Arrays.sort(nums);
 
-       for(int i=0; i<nums.length-2; i++){
-        if(i != 0 && nums[i] == nums[i-1]){
-            continue;
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i = 0; i<nums.length - 2; i++){
+            if(i != 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            else{
+                twoSum(nums, i+1, result, -nums[i]);
+            }
         }
-        twoSum(nums, i+1, result, -nums[i]);
-       }
-       return result;
+        return result;
     }
-    public void twoSum(int[] nums, int k, List<List<Integer>> result, int target){
+    public void twoSum(int[] nums, int k, List<List<Integer> >result, int target){
         int i = k;
         int j = nums.length - 1;
-        while(i<j){
-            if(nums[i] + nums[j] > target){
-                j--;
-            }
-            else if(nums[i] + nums[j] < target){
+
+        while(i < j){
+            if(nums[i] + nums[j] < target){
                 i++;
+            }
+            else if(nums[i] + nums[j] > target){
+                j--;
             }
             else{
                 result.add(Arrays.asList(nums[i], nums[j], -target));
-                while(i<j && nums[i] == nums[i+1]){
+
+                while(i < j && nums[i] == nums[i + 1]){
                     i++;
                 }
-                while(i<j && nums[j] == nums[j-1]){
+
+                while(i < j && nums[j] == nums[j - 1]){
                     j--;
                 }
                 i++;
@@ -38,3 +46,4 @@ class Solution {
         }
     }
 }
+
