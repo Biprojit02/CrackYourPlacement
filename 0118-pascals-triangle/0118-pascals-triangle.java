1c58f94069c447f3c17e65507f7573717c057659
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-07-24 01:12:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (7.21%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 1; i<=numRows; i++){
+            ans.add(pascalTri(i));
+        }
+        return ans;
+    }
+    public List<Integer> pascalTri(int row){
+        int ans = 1;
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        for(int i = 1; i<row; i++){
+            ans = ans * (row - i);
+            ans = ans/i;
+
+            list.add(ans);
+        }
+        return list;
+    }
+}
