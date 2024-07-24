ebd3e0efaf546140df47bfd754e1a51bc14fb677
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-07-24 11:20:53 +0530

    Time: 7 ms (97.26%), Space: 46.5 MB (49.78%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,8 +5,8 @@ class Solution {
         }
 
         Arrays.sort(intervals, (a, b)->Integer.compare(a[0], b[0]));
-        ArrayList<int[]> ans = new ArrayList<>();
-        int newInterval[] = intervals[0];
+        List<int[]> ans = new ArrayList<>();
+        int[] newInterval = intervals[0];
         ans.add(newInterval);
 
         for(int[] interval : intervals){
@@ -19,5 +19,5 @@ class Solution {
             }
         }
         return ans.toArray(new int[ans.size()][]);
-    }                           
+    }
 }
