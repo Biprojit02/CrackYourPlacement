c76e7f4087a7eac9f4bd114052a1b7b06b58832b
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-07-25 00:02:23 +0530

    Time: 0 ms (100%), Space: 42.3 MB (21.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,7 +4,7 @@ class Solution {
         int j = n - 1;
         int k = m + n - 1;
 
-        while(i>= 0 && j>=0){
+        while(i >= 0 && j>= 0){
             if(nums1[i] >= nums2[j]){
                 nums1[k] = nums1[i];
                 i--;
@@ -20,7 +20,7 @@ class Solution {
         while(i>=0){
             nums1[k] = nums1[i];
             i--;
-            k--; 
+            k--;
         }
 
         while(j>=0){
