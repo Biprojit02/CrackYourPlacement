20f96e19409fbde12cc691df5bc93d727fe61802
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-07-28 12:02:43 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,5 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+       int m = matrix.length;
+       int n = matrix[0].length;
+       int left = 0;
+       int right = (m * n) - 1;
+
+       while(left <= right){
+            int mid = (left + right)/2;
+            int mid_val = matrix[mid/n][mid%n];
+
+            if(mid_val == target){
+                return true;
+            } 
+            else if(mid_val < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        } 
+       return false;
+    }
+}
+
+/* 
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
         int i = 0;
         int j = matrix[0].length - 1;
 
@@ -17,3 +43,4 @@ class Solution {
         return false;
     }
 }
+*/
