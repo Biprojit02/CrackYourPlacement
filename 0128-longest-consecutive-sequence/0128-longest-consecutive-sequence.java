8b9722ce77168d3bc70503d1b46c4aabda22e828
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-07-30 04:42:50 +0530

    Time: 1081 ms (22.73%), Space: 67.5 MB (14.83%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,6 +1,34 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         if(nums.length == 0) return 0;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        int cnt = 1; 
+        int longest = 0;
+
+        for(int num : nums){
+            if(!set.contains(num - 1)){
+                int x = num;
+                cnt = 1;
+                while(set.contains(x + 1)){
+                    cnt++;
+                    x = x+1;
+                }
+            }
+            longest = Math.max(longest, cnt);
+        }
+        return longest;
+    }
+}
+/* 
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0) return 0;
         
         int count = 1;
         int maxCount = 0;
@@ -20,3 +48,4 @@ class Solution {
         return Math.max(maxCount, count);
     }
 }
+*/
