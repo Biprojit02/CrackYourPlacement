c3e98a7265260448ec5b3755d72951ca2695b983
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-08-05 11:46:24 +0530

    Time: 1051 ms (30.31%), Space: 67.1 MB (32.52%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,51 +1,24 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
-
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+        int count = 1;
+        int longestSeq = 0;
 
         for(int num : nums){
             set.add(num);
         }
 
-        int cnt = 1; 
-        int longest = 0;
-
         for(int num : nums){
             if(!set.contains(num - 1)){
                 int x = num;
-                cnt = 1;
-                while(set.contains(x + 1)){
-                    cnt++;
-                    x = x+1;
-                }
-            }
-            longest = Math.max(longest, cnt);
-        }
-        return longest;
-    }
-}
-/* 
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
-        
-        int count = 1;
-        int maxCount = 0;
-    
-        Arrays.sort(nums);
-        for(int i = 1; i<nums.length; i++){
-            if(nums[i] != nums[i-1]){
-                if(nums[i] == nums[i-1] + 1){
+                count = 1;
+                while(set.contains(x+1)){
                     count++;
-                } 
-                else{
-                    maxCount = Math.max(maxCount, count);
-                    count = 1;
+                    x++;
                 }
             }
+            longestSeq = Math.max(count, longestSeq);
         }
-        return Math.max(maxCount, count);
+        return longestSeq;
     }
 }
-*/
