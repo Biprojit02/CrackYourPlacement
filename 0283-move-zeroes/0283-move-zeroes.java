50e09e1b118d9d892df332b67d7223e27b9ad46f
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-08-12 22:25:38 +0530

    Time: 2 ms (82.75%), Space: 45.9 MB (64.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0;
         int i = 0;
+        int j = 0;
         while(i < nums.length){
             if(nums[i] == 0){
                 i++;
