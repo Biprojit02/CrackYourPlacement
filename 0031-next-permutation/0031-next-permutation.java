9fecad441ed86e0312fe582dd70c0a96f58c9f20
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-08-16 11:25:49 +0530

    Time: 0 ms (100%), Space: 42.9 MB (74.04%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,18 +1,19 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
         int circle_index = -1;
-        
-        for(int i = n - 1; i>0; i--){
+        int n = nums.length;
+
+        for(int i = n-1; i> 0; i--){
             if(nums[i] > nums[i-1]){
                 circle_index = i-1;
                 break;
             }
         }
 
+        int swap_index = -1;
+
         if(circle_index != -1){
-            int swap_index = -1;
-            for(int j = n-1; j>circle_index; j--){
+            for(int j = n-1; j>=circle_index; j--){
                 if(nums[j] > nums[circle_index]){
                     swap_index = j;
                     break;
@@ -20,18 +21,18 @@ class Solution {
             }
             swap(nums, circle_index, swap_index);
         }
-        reverse(nums, circle_index + 1, n-1);
+        reverse(nums, circle_index+1, n-1);
     }
 
-    public void swap(int nums[], int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    public void reverse(int nums[], int start, int end){
+    public void reverse(int arr[], int start, int end){
         while(start<end){
-            swap(nums, start, end);
+            swap(arr, start, end);
             start++;
             end--;
         }
