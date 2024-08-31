d86b9c7f46916001273caecccdbe42bd18b4304f
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-08-31 23:56:57 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,3 +1,38 @@
+class Solution{
+    public int search(int[] nums, int target){
+        int n = nums.length;
+        int left = 0; 
+        int right = n - 1;
+        int mid; 
+
+        while(left <= right){
+            mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] >= nums[left]){
+                if(target <= nums[mid] && target >= nums[left]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(target >= nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+//TWO PASS APPROACH
+/*
 class Solution {
     public int pivot(int[] nums, int n){
         int l = 0;
@@ -44,3 +79,4 @@ class Solution {
         return idx;
     }
 }
+*/
