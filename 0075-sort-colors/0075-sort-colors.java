fa91204626329b1b70e348a087d9b6ce40a2464a
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-09-02 19:12:26 +0530

    Time: 0 ms (100%), Space: 42 MB (27.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,14 +3,13 @@ class Solution {
         int first = 0;
         int mid = 0;
         int last = nums.length - 1;
-
         while(mid <= last){
             if(nums[mid] == 0){
                 int temp = nums[mid];
                 nums[mid] = nums[first];
                 nums[first] = temp;
-                first++;
                 mid++;
+                first++;
             }
             else if(nums[mid] == 2){
                 int temp = nums[mid];
