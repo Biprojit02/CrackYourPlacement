a180a82c4c0ac4e3c7a328b501c364a92080f440
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-09-04 01:08:32 +0530

    Time: 19 ms (43.76%), Space: 43.6 MB (94.19%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -4,12 +4,12 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
 
-        for(int i = 0; i<n; i++){
-            if(i != 0 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1; j<n; j++){
+        for(int i = 0; i<n - 3; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j<n - 2; j++){
                 if(j > i+1 && nums[j] == nums[j-1]) continue;
-                int k = j+1;
-                int l = n-1;
+                int k = j + 1;
+                int l = n - 1;
                 while(k < l){
                     long sum = (long)nums[i] + nums[j];
                     sum += nums[k];
@@ -21,11 +21,11 @@ class Solution {
                         list.add(nums[k]);
                         list.add(nums[l]);
                         ans.add(list);
+                        while(k < l && nums[k] == nums[k+1]) k++;
+                        
+                        while(k < l && nums[l] == nums[l-1]) l--;
                         k++;
                         l--;
-
-                        while(k<l && nums[k] == nums[k-1]) k++;
-                        while(k<l && nums[l] == nums[l+1]) l--;
                     }
                     else if(sum > target){
                         l--;
