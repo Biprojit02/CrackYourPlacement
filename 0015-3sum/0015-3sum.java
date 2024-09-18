581607b317f88db6a3c78b609913e926441082d5
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-09-18 15:31:26 +0530

    Time: 26 ms (92.52%), Space: 51.4 MB (66.75%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,11 +1,10 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums.length < 3){
-            return new ArrayList<>();
-        }
-
-        Arrays.sort(nums);
+        if(nums.length < 3) return new ArrayList<>();
+        
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
         for(int i = 0; i<nums.length; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
@@ -13,13 +12,13 @@ class Solution {
             else{
                 twoSum(nums, i+1, result, -nums[i]);
             }
-        } 
+        }
         return result;
     }
-
     public void twoSum(int[] nums, int k, List<List<Integer>> result, int target){
         int i = k;
         int j = nums.length - 1;
+
         while(i < j){
             int sum = nums[i] + nums[j];
             if(sum < target){
