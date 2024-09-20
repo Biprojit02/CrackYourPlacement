9f3a0a5c3dcdfaef03fffde5ab1125162b59417f
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-09-20 11:31:11 +0530

    Time: 17 ms (18.44%), Space: 43 MB (72.15%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,19 +1,19 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2];
+        int closetSum = nums[0] + nums[1] + nums[2];
+
 
         for(int i = 0; i<nums.length - 2; i++){
-            int left = i+1;
+            int left = i + 1;
             int right = nums.length - 1;
-
             while(left < right){
                 int currentSum = nums[i] + nums[left] + nums[right];
-                
-                if(Math.abs(target - currentSum) < Math.abs(target - closestSum)){
-                    closestSum = currentSum;
+
+                if(Math.abs(target - currentSum) < Math.abs(target - closetSum)){
+                    closetSum = currentSum;
                 }
-                
+
                 if(currentSum < target){
                     left++;
                 }
@@ -22,6 +22,6 @@ class Solution {
                 }
             }
         }
-        return closestSum;
+        return closetSum;
     }
 }
