bb7ea55b3232f7469b80b130254a7957d3c7484f
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-10-13 22:41:29 +0530

    Time: 2 ms (92.52%), Space: 47.1 MB (39.87%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.length - 1;
-
         while(left <= right){
             int sum = numbers[left] + numbers[right];
             if(sum < target){
@@ -12,7 +11,7 @@ class Solution {
                 right--;
             }
             else{
-                return new int[]{left+1, right+1};
+                return new int[]{left + 1, right + 1};
             }
         }
         return new int[]{-1};
