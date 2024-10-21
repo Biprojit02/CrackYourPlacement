dca6701cc6831d42b0214473878a330c9d254513
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-10-21 10:58:56 +0530

    Time: 61 ms (78.33%), Space: 57.1 MB (85.84%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
         int n = temp.length;
-        int arr[] = new int[n];
+        int[] ans = new int[n];
         Stack<Integer> st = new Stack<>();
+        
         for(int i = 0; i<n; i++){
             while(!st.isEmpty() && temp[i] > temp[st.peek()]){
                 int index = st.pop();
-                arr[index] = i - index;
+                ans[index] = i - index;
             }
             st.push(i);
         }
-        return arr;
+        return ans;
     }
 }
