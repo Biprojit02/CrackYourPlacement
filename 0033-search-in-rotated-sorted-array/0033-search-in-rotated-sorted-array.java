6866c58cf9e7612a3cec3f10f43db18b9a775e52
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-10-25 18:36:53 +0530

    Time: 0 ms (100%), Space: 42.1 MB (41.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,61 +1,27 @@
-class Solution{
-    public int search(int[] nums, int target){
-        int n = nums.length;
-        int left = 0; 
-        int right = n - 1;
-        int mid; 
-
-        while(left <= right){
-            mid = left + (right - left)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] >= nums[left]){
-                if(target <= nums[mid] && target >= nums[left]){
-                    right = mid - 1;
-                }
-                else{
-                    left = mid + 1;
-                }
-            }
-            else{
-                if(target >= nums[mid] && target <= nums[right]){
-                    left = mid + 1;
-                }
-                else{
-                    right = mid - 1;
-                }
-            }
-        }
-        return -1;
-    }
-}
-
-//TWO PASS APPROACH
-/*
+//TWO PASS
 class Solution {
+    int idx = -1;
     public int pivot(int[] nums, int n){
-        int l = 0;
-        int r = n - 1;
-        while(l < r){
-            int mid = l + (r - l)/2;
-            if(nums[mid] > nums[r]){
-                l = mid + 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] >= nums[right]){
+                left = mid + 1;
             }
             else{
-                r = mid;
+                right = mid;
             }
         }
-        return r;
+        return right;
     }
 
     public int binarySearch(int[] nums, int left, int right, int target){
-        int idx = -1;
         while(left <= right){
             int mid = left + (right - left)/2;
             if(nums[mid] == target){
                 idx = mid;
-                break;
+                return idx;
             }
             else if(nums[mid] > target){
                 right = mid - 1;
@@ -68,15 +34,15 @@ class Solution {
     }
     public int search(int[] nums, int target) {
         int n = nums.length;
-       
+        int left = 0;
+        int right = n - 1;
         int pivot = pivot(nums, n);
-        int idx = binarySearch(nums, 0 , pivot - 1, target);
+        binarySearch(nums, left, pivot - 1, target);
         if(idx != -1){
             return idx;
         }
 
-        idx = binarySearch(nums, pivot, n - 1, target);
+        binarySearch(nums, pivot, right, target);
         return idx;
     }
 }
-*/
