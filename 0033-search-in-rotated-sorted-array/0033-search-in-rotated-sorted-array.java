a0d5bd2d12412a6a4ddc0eaf1965ed35b3d6a309
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-10-25 18:45:13 +0530

    Time: 0 ms (100%), Space: 42.1 MB (41.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,48 +1,31 @@
-//TWO PASS
+//ONE PASS
 class Solution {
-    int idx = -1;
-    public int pivot(int[] nums, int n){
-        int left = 0;
-        int right = nums.length - 1;
-        while(left < right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] >= nums[right]){
-                left = mid + 1;
-            }
-            else{
-                right = mid;
-            }
-        }
-        return right;
-    }
-
-    public int binarySearch(int[] nums, int left, int right, int target){
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target){
-                idx = mid;
-                return idx;
-            }
-            else if(nums[mid] > target){
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
-        }
-        return idx;
-    }
     public int search(int[] nums, int target) {
         int n = nums.length;
         int left = 0;
         int right = n - 1;
-        int pivot = pivot(nums, n);
-        binarySearch(nums, left, pivot - 1, target);
-        if(idx != -1){
-            return idx;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] >= nums[left]){
+                if(target < nums[mid] && target >= nums[left]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(target<=nums[right] && target >= nums[mid]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
         }
-
-        binarySearch(nums, pivot, right, target);
-        return idx;
+        return -1;
     }
 }
