c9f37072d8445a60220fc55a6e09b88eca9f3f93
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-10-28 22:25:54 +0530

    Time: 2 ms (88.83%), Space: 55.1 MB (87.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,5 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans  = new int[n];
+
+        ans[0] = 1; 
+        for(int i = 1; i<n; i++){
+            ans[i] = ans[i-1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for(int i = n - 1; i>=0; i--){
+            ans[i] = ans[i] * right;
+            right *= nums[i];
+        } 
+
+        return ans;
+    }
+}
+
+/*
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
         int productWithoutZero = 1;
         int countZero = 0;
 
@@ -39,3 +60,4 @@ class Solution {
         return nums;
     }
 }
+*/
