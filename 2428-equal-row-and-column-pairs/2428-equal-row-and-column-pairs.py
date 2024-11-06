68e8a66bca16224e9a87be2455b46d347e420e6c
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-11-06 11:37:17 +0530

    Time: 79 ms (41.07%), Space: 21.4 MB (70.08%) - LeetHub

---
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -0,0 +1,31 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        length = len(grid)
+        map1 = defaultdict(int)
+        map2 = defaultdict(int)
+
+        for i in range(length):
+            s = []
+            for j in range(length):
+                s.append(str(grid[i][j]))
+                s.append(":")
+            str_row = "".join(s)
+            map1[str_row] += 1
+
+        for i in range(length):
+            s = []
+            for j in range(length):
+                s.append(str(grid[j][i]))
+                s.append(":")
+            str_col = "".join(s)
+            map2[str_col] += 1
+        
+        ans = 0
+        for s in map1.keys():
+            if s in map2:
+                ans += map1[s] * map2[s]
+
+        return ans
+        
+
+
