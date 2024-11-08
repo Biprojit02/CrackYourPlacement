4e19e38d83a97e140b2fd155d7d255e88286eb1e
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-11-09 00:31:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (9.12%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,58 +1,41 @@
-// ONE PASS APPROACH
-class Solution{
-    public ListNode removeNthFromEnd(ListNode head, int n){
-        ListNode temp = head;
-        for(int i = 0; i<n; i++){
-            temp = temp.next;
-        }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 
-        if(temp == null){
-            ListNode res = head.next;
-            head.next = null;
-            return res;
-        }
-
-        ListNode prev = head;
-        while(temp != null && temp.next != null){
-            prev = prev.next;
-            temp = temp.next;
-        }
-        prev.next = prev.next.next;
-        return head;
-    }
-}
-
-/*
- // TWO PASS APPROACH
+ //Twopass
 class Solution {
     public int lengthOfLL(ListNode head){
-        ListNode temp = head;
         int count = 0;
+        ListNode temp = head;
         while(temp != null){
-            temp = temp.next;
             count++;
+            temp = temp.next;
         }
         return count;
     }
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int l = lengthOfLL(head);
-        if(l == n){
+        int lengthOfLL = lengthOfLL(head);
+        if(lengthOfLL == n){
             ListNode temp = head.next;
             head.next = null;
             return temp;
         }
 
-        int travel_length = l - n;
+        int travelLength = lengthOfLL - n;
         ListNode prev = null;
         ListNode temp = head;
-
-        while(travel_length-- > 0){
+        while(travelLength-- > 0){
             prev = temp;
             temp = temp.next;
         }
-
         prev.next = temp.next;
         return head;
     }
 }
-*/
