dfcf12aefbf81509e49633f8dfa9316d011fcd58
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-11-10 01:09:14 +0530

    Time: 1 ms (14.32%), Space: 42.1 MB (7.84%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,16 +13,29 @@
  *     }
  * }
  */
+// BFS
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        return symmetric(root.left, root.right);
-    }
-    public boolean symmetric(TreeNode N1, TreeNode N2){
-        if(N1 == null && N2 == null) return true;
-        if(N1 == null || N2 == null) return false;
-        
-        return N1.val == N2.val && symmetric(N1.left, N2.right) && symmetric(N1.right, N2.left);
+
+        Queue<TreeNode> leftSubtree =  new LinkedList<>();
+        Queue<TreeNode> rightSubtree =  new LinkedList<>();
+
+        leftSubtree.add(root.left);
+        rightSubtree.add(root.right);
+
+        while(!leftSubtree.isEmpty() && !rightSubtree.isEmpty()){
+            TreeNode left = leftSubtree.poll();
+            TreeNode right = rightSubtree.poll();
+            if(left == null && right == null) continue;
+            if(left == null || right == null) return false;
+            if(left.val != right.val) return false;
+
+            leftSubtree.add(left.left);
+            leftSubtree.add(left.right);
+            rightSubtree.add(right.right);
+            rightSubtree.add(right.left);
+        }
+        return true;
     }
 }
-
