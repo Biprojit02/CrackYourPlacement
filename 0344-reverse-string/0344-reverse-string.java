933909f0fc3af2c3106be510be0bb579e55390ac
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-11-17 04:42:57 +0530

    Time: 1 ms (24.6%), Space: 50.4 MB (5.14%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -2,6 +2,27 @@ class Solution {
     public void reverseString(char[] s) {
         int start = 0;
         int end = s.length - 1;
+        helper(start, end, s);
+    }
+
+    public void helper(int start, int end, char[] s){
+        if(start > end) return;
+        swap(start, end, s);
+        helper(start+1, end-1, s);
+    }
+
+    public void swap(int start, int end, char[] s){
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+    }
+}
+
+/*
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
 
         while(start <= end){
             char temp = s[start];
@@ -12,3 +33,4 @@ class Solution {
         }
     }
 }
+*/
