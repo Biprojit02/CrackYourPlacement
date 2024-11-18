6c54fbc2bcb1c19cc3bf4860c91a495130afdebf
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-11-18 18:47:50 +0530

    Time: 37 ms (33.19%), Space: 54.8 MB (10.94%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -3,29 +3,32 @@ class Solution {
         int n = grid.length;
         HashMap<String, Integer> map1 = new HashMap<>();
         HashMap<String, Integer> map2 = new HashMap<>();
-        for(int i = 0; i < n; i++) {
-            StringBuilder s = new StringBuilder();
-            for(int j = 0; j < n; j++) {
-                s.append(grid[i][j]);
-                s.append(":");
+
+        for(int i = 0; i<n; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j<n; j++){
+                sb.append(grid[i][j]);
+                sb.append(":");
             }
-            String str = s.toString();
-            map1.put(str, map1.getOrDefault(str, 0) + 1);
+            String str = sb.toString();
+            map1.put(str, map1.getOrDefault(str, 0)+1);
         }
-        for(int i = 0; i < n; i++) {
-            StringBuilder s = new StringBuilder();
-            for(int j = 0; j < n; j++) {
-                s.append(grid[j][i]);
-                s.append(":");
+
+        for(int i = 0; i<n; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j<n; j++){
+                sb.append(grid[j][i]);
+                sb.append(":");
             }
-            String str = s.toString();
-            map2.put(str, map2.getOrDefault(str, 0) + 1);
+            String str = sb.toString();
+            map2.put(str, map2.getOrDefault(str, 0)+1);
         }
+
         int ans = 0;
-        for(String s : map1.keySet()) {
-            if(map2.containsKey(s)) {
-                ans += map1.get(s) * map2.get(s);
-            }
+        for(String st : map1.keySet()){
+            if(map2.containsKey(st)){
+                ans += map1.get(st) * map2.get(st);
+            }            
         }
         return ans;
     }
