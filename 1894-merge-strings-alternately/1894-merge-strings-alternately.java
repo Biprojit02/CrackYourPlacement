6a98858fb548f66e0549247880e987dd9ca11318
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2024-12-01 23:55:34 +0530

    Time: 1 ms (83.83%), Space: 41.8 MB (40.7%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,25 +1,35 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        if(word1.length() == 0){
+            return word2;
+        }
+
+        if(word2.length() == 0){
+            return word1;
+        }
+
         int i = 0;
         int j = 0;
-        StringBuilder ans = new StringBuilder();
-
+        StringBuilder result = new StringBuilder();
         while(i < word1.length() && j < word2.length()){
-            ans.append(word1.charAt(i));
-            ans.append(word2.charAt(j));
+            result.append(word1.charAt(i));
+            result.append(word2.charAt(j));
+
             i++;
             j++;
-        } 
+        }
 
         while(i < word1.length()){
-            ans.append(word1.charAt(i));
+            result.append(word1.charAt(i));
             i++;
         }
 
         while(j < word2.length()){
-            ans.append(word2.charAt(j));
+            result.append(word2.charAt(j));
             j++;
         }
-        return ans.toString();
+        return result.toString();
     }
 }
+
+
