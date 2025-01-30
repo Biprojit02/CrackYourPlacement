8940f20ac780ff4cd09ea1eff0eda3fe3aaab2e3
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-01-30 17:40:18 +0530

    Time: 2 ms (88.49%), Space: 56.4 MB (16.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,37 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int productWithoutZero = 1;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] != 0){
-                productWithoutZero *= nums[i];
-            }
+        int n = nums.length; 
+        int left[] = new int[n];
+        int right[] = new int[n];
+        left[0] = 1;
+        right[n-1] = 1;
+        
+        for(int i = 1; i<n; i++){
+            left[i] = nums[i-1] * left[i-1];
         }
 
-        int countZero = 0;
-        for(int n : nums){
-            if(n == 0){
-                countZero++;
-            }
+        for(int i = n-2; i>=0; i--){
+            right[i] = nums[i+1] * right[i+1];
         }
 
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] != 0){
-                if(countZero > 0){
-                    nums[i] = 0;
-                }
-                else{
-                    nums[i] = productWithoutZero / nums[i];
-                }
-            }
-            else{
-                if(countZero > 1){
-                    nums[i] = 0;
-                }
-                else{
-                    nums[i] = productWithoutZero;
-                }
-            }
+        int result[] = new int[n];
+        for(int i = 0; i<n; i++){
+            result[i] = left[i] * right[i];
         }
-        return nums;
+
+        return result;
     }
 }
