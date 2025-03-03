6f969902c71f867bd6ee90d87521d76d79905313
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-03-04 01:36:49 +0530

    Time: 25 ms (95.11%), Space: 51.3 MB (92.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,6 +1,8 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums.length < 3) return new ArrayList<>();
+        if(nums.length < 3){
+            return new ArrayList<>();
+        }
         Arrays.sort(nums);
 
         List<List<Integer>> result = new ArrayList<>();
@@ -11,7 +13,7 @@ class Solution {
             else{
                 twoSum(nums, i+1, result, -nums[i]);
             }
-        }
+        } 
         return result;
     }
 
@@ -19,20 +21,17 @@ class Solution {
         int i = k;
         int j = nums.length - 1;
         while(i < j){
-            //sum of two pointers
             int sum = nums[i] + nums[j];
-            //check the sum is greater than the target or not
-            if(sum < target){
-                i++;
-            }
-            else if(sum > target){
+            if(sum > target){
                 j--;
             }
-            //if the sum is equal to target than add to the list
+            else if(sum < target){
+                i++;
+            }
             else{
                 result.add(Arrays.asList(nums[i], nums[j], -target));
-                while(i<j && nums[i] == nums[i+1]) i++;
-                while(i<j && nums[j] == nums[j-1]) j--;
+                while(i < j && nums[i] == nums[i+1]) i++;
+                while(i < j && nums[j] == nums[j-1]) j--;
                 i++;
                 j--;
             }
