1eec03aa6d17db17081fc46fc63895fe4bb4ae6b
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-03-13 01:17:14 +0530

    Time: 0 ms (100%), Space: 41.6 MB (62.56%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,6 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         Arrays.sort(strs);
+
         String s1 = strs[0];
         String s2 = strs[strs.length - 1];
         int idx = 0;
@@ -12,6 +13,6 @@ class Solution {
                 break;
             }
         }
-        return s1.substring(0, idx); 
+        return s1.substring(0, idx);
     }
 }
