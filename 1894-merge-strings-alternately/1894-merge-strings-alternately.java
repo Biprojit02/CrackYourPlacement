1a7287cd32599644fc5c54fc3c1a7757d386bc5f
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-03-14 00:25:59 +0530

    Time: 1 ms (82.95%), Space: 41.6 MB (85.05%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,32 +1,25 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        // Initialize indices for both strings
-        int indexFirst = 0;
-        int indexSecond = 0;
-        // Use StringBuilder to efficiently build the merged string
-        StringBuilder mergedString = new StringBuilder();
-        
-        // Alternate adding characters from both strings
-        while (indexFirst < word1.length() && indexSecond < word2.length()) {
-            mergedString.append(word1.charAt(indexFirst)); // Append character from word1
-            mergedString.append(word2.charAt(indexSecond)); // Append character from word2
-            indexFirst++; // Move to the next character in word1
-            indexSecond++; // Move to the next character in word2
-        } 
-        
-        // Append remaining characters from word1, if any
-        while (indexFirst < word1.length()) {
-            mergedString.append(word1.charAt(indexFirst));
-            indexFirst++; // Move to the next character in word1
+        StringBuilder str = new StringBuilder();
+        int w1 = 0;
+        int w2 = 0;
+        while(w1 < word1.length() && w2 < word2.length()){
+            str.append(word1.charAt(w1));
+            str.append(word2.charAt(w2));
+            w1++;
+            w2++;
         }
-        
-        // Append remaining characters from word2, if any
-        while (indexSecond < word2.length()) {
-            mergedString.append(word2.charAt(indexSecond));
-            indexSecond++; // Move to the next character in word2
+
+        while(w1 < word1.length()){
+            str.append(word1.charAt(w1));
+            w1++;
         }
-        
-        // Return the final merged string
-        return mergedString.toString();
+
+        while(w2 < word2.length()){
+            str.append(word2.charAt(w2));
+            w2++;
+
+        }
+        return str.toString();
     }
 }
