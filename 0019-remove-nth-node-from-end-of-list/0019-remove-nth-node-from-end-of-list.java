10da722c2b6b44ee66ea074fc745848b19f2b20c
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-03-25 01:06:42 +0530

    Time: 0 ms (100%), Space: 41.9 MB (32.79%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,21 +1,10 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
- //One pass
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+// ONE PASS APPROACH
+class Solution{
+    public ListNode removeNthFromEnd(ListNode head, int n){
         ListNode temp = head;
-        for(int i = 0; i<n; i++){
+       for(int i = 0; i<n; i++){
             temp = temp.next;
-        }
+       }
 
         if(temp == null){
             ListNode res = head.next;
@@ -23,13 +12,50 @@ class Solution {
             return res;
         }
 
-        ListNode prev = head;
-        while(temp != null && temp.next != null){
+       ListNode prev = head;
+       while(temp != null && temp.next != null){
             prev = prev.next;
             temp = temp.next;
+       }
+
+       prev.next = prev.next.next;
+       return head;
+    }
+}
+
+/*
+ // TWO PASS APPROACH
+class Solution {
+    public int lengthOfLL(ListNode head){
+        ListNode temp = head;
+        int count = 0;
+        while(temp != null){
+            temp = temp.next;
+            count++;
+        }
+        return count;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int l = lengthOfLL(head);
+        if(l == n){
+            ListNode temp = head.next;
+            head.next = null;
+            return temp;
         }
 
-        prev.next = prev.next.next;
+        int travel_length = l - n;
+        ListNode prev = null;
+        ListNode temp = head;
+
+        while(travel_length-- > 0){
+            prev = temp;
+            temp = temp.next;  1
+        }
+
+        prev.next = temp.next;
         return head;
     }
 }
+*/
+
+
