69a9ba870ca0dcfa4bfc6884cda79a624694c490
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-03-28 01:34:00 +0530

    Time: 13 ms (21.17%), Space: 47.6 MB (80.13%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,21 +4,20 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         ArrayList<Integer> list = new ArrayList<>();
         int n = nums.length;
-
-        for(int i = 0; i<n; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for(int i = 0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
 
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<nums.length; i++){
             if(map.get(nums[i]) > n/3){
                 set.add(nums[i]);
             }
         }
-        
+
         for(int val : set){
             list.add(val);
         }
-        
         return list;
     }
 }
+
