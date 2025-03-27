cc324ad6346d21770a1037564075eba54bceb63b
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-03-28 01:45:51 +0530

    Time: 27 ms (85.06%), Space: 51.4 MB (71.4%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,8 +3,8 @@ class Solution {
         if(nums.length < 3){
             return new ArrayList<>();
         }
-        Arrays.sort(nums);
 
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i<nums.length; i++){
             if(i > 0 && nums[i] == nums[i-1]){
@@ -13,27 +13,27 @@ class Solution {
             else{
                 twoSum(nums, i+1, result, -nums[i]);
             }
-        } 
+        }
         return result;
-    }
 
+    }
     public void twoSum(int[] nums, int k, List<List<Integer>> result, int target){
-        int i = k;
-        int j = nums.length - 1;
-        while(i < j){
-            int sum = nums[i] + nums[j];
-            if(sum > target){
-                j--;
+        int start = k;
+        int end = nums.length - 1;
+        while(start < end){
+            int sum = nums[start] + nums[end];
+            if(sum < target){
+                start++;
             }
-            else if(sum < target){
-                i++;
+            else if(sum > target){
+                end--;
             }
             else{
-                result.add(Arrays.asList(nums[i], nums[j], -target));
-                while(i < j && nums[i] == nums[i+1]) i++;
-                while(i < j && nums[j] == nums[j-1]) j--;
-                i++;
-                j--;
+                result.add(Arrays.asList(nums[start], nums[end], -target));
+                while(start < end && nums[start] == nums[start + 1]) start++;
+                while(start < end && nums[end] == nums[end - 1]) end--;
+                start++;
+                end--;
             }
         }
     }
