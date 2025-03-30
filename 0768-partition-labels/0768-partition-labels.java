410f2a96b01d33d0d50f46fbdc504477e217c3cf
Biprojit Giri <98759448+Biprojit02@users.noreply.github.com>
2025-03-31 00:53:28 +0530

    Time: 8 ms (49.76%), Space: 42.4 MB (43.2%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int n = s.length();
+        List<Integer> l = new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        //Store the last indexes of the characters.
+        for (int i = 0; i < n; i++) map.put(s.charAt(i), i);
+
+        int outerLoop = 0;
+        while (outerLoop < n) {
+            int maxEnd = map.get(s.charAt(outerLoop)), innerLoop = outerLoop + 1;
+            while (innerLoop < maxEnd) {
+                int curr = map.get(s.charAt(innerLoop));
+                if (curr > maxEnd) {
+                    maxEnd = curr;
+                }
+                innerLoop++;
+            }
+            l.add(maxEnd - outerLoop + 1);
+            outerLoop = maxEnd + 1;
+        }
+
+        return l;
+    }
+}
